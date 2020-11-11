3bd6941a8ab764c6a73f59ec114258db39ed0dcc
Jesse <jessemckinney513@gmail.com>
2020-11-11 15:41:34 -0500

    yay

---
+++ client/src/pages/Posts.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+class Books extends Component {
+  state = {
+    posts: [],
+    userName: "",
+    userPost: ""
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = () => {
+    API.getUserPosts()
+      .then(res =>
+        this.setState({ posts: res.data, userName: "", userPost: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteUserPost = id => {
+    API.deleteUserPost(id)
+      .then(res => this.loadPosts())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.userName) {
+      API.saveUserPost({
+        userName: this.state.userName,
+        userPost: this.state.userPost
+      })
+        .then(res => this.loadPosts())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>What Books Should I Read?</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                value={this.state.userName}
+                onChange={this.handleInputChange}
+                name="userName"
+                placeholder="Username (required)"
+              />
+              <TextArea
+                value={this.state.userPost}
+                onChange={this.handleInputChange}
+                name="userPost"
+                placeholder="Post (Optional)"
+              />
+              <FormBtn
+                disabled={!(this.state.userName)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Post
+              </FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Posts On My List</h1>
+            </Jumbotron>
+            {this.state.posts.length ? (
+              <List>
+                {this.state.posts.map(book => (
+                  <ListItem key={post._id}>
+                    <Link to={"/posts/" + post._id}>
+                      <strong>
+                        {post.userName} says:
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteUserPost(post._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
