065783bb665412cddeb163a640087a0d9f5b7ab5
Jesse <jessemckinney513@gmail.com>
2020-11-18 12:40:02 -0500

    yay

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,24 +1,69 @@
-import React from "react";
-import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Style from "./NoMatch.module.css";
 
-function NoMatch() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
-          </Jumbotron>
-        </Col>
-      </Row>
-    </Container>
-  );
+class Error extends Component {
+  render() {
+    return (
+      <>
+        <div className={Style.errorWrapper}>
+          <Container className={Style.eCon}>
+            <Row className={Style.eRow}>
+              <Col className={Style.eCol}>
+                <div className={Style.eh1}>ERROR 1203499C0005ƆB</div>
+              </Col>
+            </Row>
+            <Row className={Style.eRow}>
+              <div className={Style.eh2}>
+                Wellp, now you did it. Broke the damn computer. You probably
+                didn't even do anything major, did you? But see, I'm the dreaded
+                Blue Screen of Death, and I pop up totally randomly, and most
+                times, for no good gah-darn reason.
+              </div>
+            </Row>
+            <Row className={Style.eRow}>
+              <div className={Style.eh2}>
+                You're probably sweatin' bullets right now, aren't you? I love
+                it. So, there are a couple options you could try, neither of
+                which will work whatsoever, but heck, be my guest.
+              </div>
+            </Row>
+            <Row className={Style.eRow}>
+              <ul>
+                <li>
+                  <div className={Style.eh2}>
+                    Hit CTRL+ALT+DEL. This will restart me. But, everything
+                    you've done on me up until this point will be gone. Tough
+                    CRUD. But if you're into downloading weird online GIF's
+                    (which you are), I'm guessing this isn't a terrible option.
+                    Pervert.
+                  </div>
+                </li>
+                <br />
+                <li>
+                  <div className={Style.eh2}>
+                    Don't hit CTRL+ALT+DEL and get on the phone with every
+                    computer repair guy in town. All of 'em. Give'em all a shot.
+                    It's fine. I'll wait. I ain't going anywhere. I can stay
+                    like this alllllllllll daaaaaaaayyyyyyyy...
+                  </div>
+                </li>
+              </ul>
+            </Row>
+            <Row>
+              <Link to="/">
+                <Button className={Style.HomeButton}>Home</Button>
+              </Link>
+            </Row>
+          </Container>
+        </div>
+      </>
+    );
+  }
 }
 
-export default NoMatch;
+export default Error;
