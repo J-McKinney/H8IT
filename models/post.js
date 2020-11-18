29aa930a7c97f415efc0b4139fa5fec774ad0ba0
Jesse <jessemckinney513@gmail.com>
2020-11-18 10:18:25 -0500

    yay got the expire thing in seconds working

--- models/post.js
+++ models/post.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   userName: { type: String, required: true },
   userPost: String,
-  date: { type: Date, default: Date.now },
+  dateCreated: { type: Date, default: Date.now },
+  expire_at: { type: Date, default: Date.now, expires: 60 }, // the document expires after however many seconds
 });
 
 const Post = mongoose.model("Post", postSchema);
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const postRoutes = require("./posts");
 
-// Book routes
+// Post routes
 router.use("/posts", postRoutes);
 
 module.exports = router;
